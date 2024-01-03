7cb8400720022e34804c9923d79b5d56a6e98dc0
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-01-03 15:39:18 +0100

    Footer

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 import Header from './Components/header'
 import Catalogo from "./Components/catalogo";
-import Footer from "./Components/footer"
+import Footer from "./Components/footer";
 function App() {
   
 
   return (
     <>
       <Header />
-  <Catalogo />
-  <Footer />
+      <Catalogo />
+      <Footer />
     </>
   )
 }
--- netflix/src/Components/footer.jsx
+++ netflix/src/Components/footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+
+
 import "./Footer.css";
 
 const Footer = () => {
@@ -8,30 +8,16 @@ const Footer = () => {
         <footer>
             <section className="socialNetwork">
                 <a href="https://www.facebook.com/tupagina" target="_blank" rel="noopener noreferrer">
-                    <FaFacebook />
+                ddd
                 </a>
                 <a href="https://twitter.com/tucuenta" target="_blank" rel="noopener noreferrer">
-                    <FaTwitter />
+                  ddd
                 </a>
                 <a href="https://www.instagram.com/tucuenta" target="_blank" rel="noopener noreferrer">
-                    <FaInstagram />
+                ddd
                 </a>
             </section>
-            <section className="menu">
-                <Link to="/audiodescripcion">Audiodescripción</Link>
-                <Link to="/centroDeAyuda">Centro de ayuda</Link>
-                <Link to="/tarjetasRegalo">Tarjetas regalo</Link>
-                <Link to="/zonaDePrensa">Zona de prensa</Link>
-                <Link to="/inversores">Inversores</Link>
-                <Link to="/empleo">Empleo</Link>
-                <Link to="/terminosdeuso">Términos de uso</Link>
-                <Link to="/privacidad">Privacidad</Link><Link to="/menu-item-1">Menú Item 1</Link>
-                <Link to="/avisosLegales">Avisos legales</Link><Link to="/menu-item-1">Menú Item 1</Link>
-                <Link to="/preferenciasDeCookies">Preferencias de cookies</Link>
-                <Link to="/informacionCorporativa">Información corporativa</Link>
-                <Link to="/contactanos">Contáctanos</Link>
-                <Link to="/opcionesDeAnuncios">Opciones de anuncios</Link>
-            </section>
+          
             <p>© 1997-2024 Netflix, Inc.</p>
         </footer>
     );
