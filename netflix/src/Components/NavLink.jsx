1c75dfd0aa4b58dcf36fcb677eb8be24ad2d49d8
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-03 12:05:05 +0100

    Creado componente header y navlink

---
+++ netflix/src/Components/NavLink.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+import './NavLink.css';
+
+export default function NavLink ({name}) {
+
+    return (
+       <a href="">{name}</a>
+    );
+}
---
+++ netflix/src/Components/header.jsx
@@ -0,0 +1,17 @@
+import './Header.css';
+import NavLink  from "./NavLink";
+
+export default function Header () {
+
+
+    return (
+        <header>
+            <img src="" alt="" />
+            <nav>
+                <NavLink name='Inicio' />
+                <NavLink name="Series" />
+                <NavLink name="Películas" />
+            </nav>
+        </header>
+    );
+}
