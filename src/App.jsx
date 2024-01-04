4338abc4fbef00858ef95f3d8b7d067d7353faa3
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-04 13:14:32 +0100

    router implementado y añadido componente series

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import './App.css';
-import Header from './Components/Header/header'
+import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Header from "./Components/Header/header";
 import Catalogo from "./Components/Catalogo/catalogo";
 import Footer from "./Components/Footer/footer";
-function App() {
-  
+import Series from "./Components/Series/Series";
 
+function App() {
   return (
     <>
-      <Header />
-      <Catalogo />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Catalogo />} />
+          <Route exact path="/Series" element={<Series />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Nav/NavLink.jsx
+++ src/Components/Nav/NavLink.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
 import './NavLink.css';
+import { Link } from "react-router-dom";
+
 
 export default function NavLink ({name}) {
 
     return (
-       <h5>{name}</h5>
+       <Link to="Series">{name}</Link>
     );
 }
