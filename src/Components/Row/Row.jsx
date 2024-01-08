f977cea5fc47423809ac606a3c66fdd45adbed39
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-01-08 09:56:30 +0100

    Generos de peliculas

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from 'react';
 import './Row.css'
 
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODU5ZDU5MzFiNThhZWIwNGQ1NzE0ZDIxZTJhZDM4ZSIsInN1YiI6IjY1OTNlYzVhYTU4OTAyNzExOTk3NmNmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RHDZ7xzcrZG0xcGzzX24WKxHEv6w9wPNOCxVt0pM8SE'
+  }
+};
+
+async function obtenerPeliculasPorGenero(genreId) {
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=&with_genres=${genreId}`, options);
+    const data = await response.json();
+    return data.results;
+}
 
 const Row = () => {
     const generosDePeliculas = {
@@ -8,14 +22,39 @@ const Row = () => {
         drama: 18,
         cienciaFiccion: 878,
         aventura: 12,
-        // Agrega más géneros según sea necesario
-      };
-      
-      
-       
-  return (
-    <p>Hola</p>
-  )
-}
+    };
+
+    const [peliculas, setPeliculas] = useState({});
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const newMovies = {};
+            for (const genre in generosDePeliculas) {
+                const genreId = generosDePeliculas[genre];
+                newMovies[genre] = await obtenerPeliculasPorGenero(genreId);
+            }
+            setPeliculas(newMovies);
+        };
+
+        fetchMovies();
+    }, []);
+
+    return (
+        <div>
+            {Object.keys(peliculas).map((genre) => (
+                <div key={genre}>
+                    <h2>{genre}</h2>
+                    {peliculas[genre].map((movie) => (
+                        <img
+                            key={movie.id}
+                            src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                            alt={movie.title}
+                        />
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default Row;
