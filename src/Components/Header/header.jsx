be2eb5194dfa1fd52e3d7b4125a4c96f1fe4ae6b
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-01-10 11:53:24 +0100

    lupa

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,6 +1,7 @@
 import "./header.css";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logoNetflix.png";
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Header() {
   return (
@@ -12,6 +13,10 @@ export default function Header() {
           <Link to="Series" className="navLink">Series</Link>
           <Link to="Peliculas" className="navLink">Películas</Link>
         </nav>
+        <div className="search">
+          <SearchIcon className="icon" />
+          <input type="text" placeholder="Buscar" />
+        </div>
       </section>
     </header>
   );
